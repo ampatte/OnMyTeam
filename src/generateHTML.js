adbe99f5f5cbfee28435ab94f26912a566710d58
ampatte <ampatterson1@hotmail.com>
2023-01-29 00:45:15 -0500

    added styling with Bootstrap

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,14 +5,14 @@ function generateHTML(team) {
 
   const managers = team.filter((employee)=>employee.getRole() === "Manager")
   .map((manager)=> 
-  `<div class="col card primary">        
-        <div class="card-header"><h2>${manager.getRole()}</h2></div>
-            <div class="card-body">
-                <h2 class="display-4">${manager.name}</h2>
+  `<div class="col card">        
+        <div class="card-header manager"><h2>${manager.getRole()}</h2>            
+            <h1 class="display-4">${manager.name}</h1></div>
+                <div class="card-body">
                     <ul class="list-group-flush">
-                        <li class="list-group-item">ID Badge # ${manager.id}</li>
-                    <a href="">${manager.email}</a>
-                <li class="list-group-item">Office Phone: ${manager.office}</li>
+                        <li class="list-group-item">ID: ${manager.id}</li>
+                    <a class="link" href="#">Email: ${manager.email}</a>
+                <li class="list-group-item">Office number: ${manager.office}</li>
             </ul>
         </div>
     </div>`)
@@ -21,14 +21,14 @@ function generateHTML(team) {
 
   const engineers = team.filter((employee)=>employee.getRole() === "Engineer")
   .map((engineer)=> 
-  `<div class="col card warning">
-        <div class="card-header"><h2> ${engineer.getRole()}</h2></div>
-            <div class="card-body">
-                <h1 class="display-4">${engineer.name}</h1>
+  `<div class="col card">
+        <div class="card-header engineer"><h2> ${engineer.getRole()}</h2>            
+            <h1 class="display-4">${engineer.name}</h1></div>
+                <div class="card-body">
                     <ul class="list-group-flush">
-                        <li class="list-group-item">ID Badge # ${engineer.id}</li>
-                    <a href="${engineer.email}">${engineer.email}</a>
-                <li class="list-group-item">Github Username: ${engineer.github}</li>
+                        <li class="list-group-item">ID: ${engineer.id}</li>
+                    <a class="link" href="#">Email: ${engineer.email}</a>
+                <li class="list-group-item">Github: ${engineer.github}</li>
             </ul>
         </div>
     </div>`)
@@ -37,13 +37,13 @@ function generateHTML(team) {
 
   const interns = team.filter((employee)=>employee.getRole() === "Intern")
   .map((intern)=> 
-  `<div class="col card info">
-        <div class="card-header"><h2>${intern.getRole()}</h2></div>
-            <div class="card-body">
-                <h1 class="display-4">${intern.name}</h1>
+  `<div class="col card">
+        <div class="card-header intern"><h2> ${intern.getRole()} </h2>
+            <h1 class="display-4">${intern.name}</h1></div>
+                <div class="card-body">
                     <ul class="list-group-flush">
-                        <li class="list-group-item">ID badge # ${intern.id}</li>
-                    <a href="${intern.email}">${intern.email}</a>
+                        <li class="list-group-item">ID: ${intern.id}</li>
+                    <a class="link" href="#">Email: ${intern.email}</a>
                 <li class="list-group-item">School: ${intern.school}</li>
             </ul>
         </div>
@@ -58,19 +58,22 @@ function generateHTML(team) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
         <title>OnMyTeam</title>
     </head>
     <body>
     <body>
-        <header class="pb-3 mb-4 border-bottom">
-            <nav class="nav navbar-fixed-top"> 
-                <h3><strong>Team Profile Generator</strong><h3>
-            </nav> 
-        </header> 
-        <div class="container-fluid" class="row align-items-center">   
-            <div id="employees"></div>
-             ${teamHTML.join("")}
+        <header class=" top pb-5 mb-4" class="header">
+            <h1>Team Profile Generator<h1>
+                </header> 
+            <div class="container-fluid " class="row align-items-center">   
+        <div id="employees">
+                ${teamHTML.join("")}
+            </div>
         </div>
     </body>
     </html>`
